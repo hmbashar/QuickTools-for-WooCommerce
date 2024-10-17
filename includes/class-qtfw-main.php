9d08fd52e6d85450ed18888fc38ca436374e74d4
hmbashar <hmbashar@gmail.com>
2024-10-17 19:48:40 +0600

    added style

--- includes/class-qtfw-main.php
+++ includes/class-qtfw-main.php
@@ -21,19 +21,12 @@ class Main {
 
     // Define hooks and actions
     private function define_hooks() {
-        add_action('admin_enqueue_scripts', [$this, 'enqueue_admin_assets']);
         add_action('wp_enqueue_scripts', [$this, 'enqueue_public_assets']);
     }
 
-    // Enqueue admin scripts and styles
-    public function enqueue_admin_assets() {
-        wp_enqueue_style('qtfw-admin', QTFW_PLUGIN_URL . 'assets/css/admin.css', [], QTFW_VERSION);
-        wp_enqueue_script('qtfw-admin', QTFW_PLUGIN_URL . 'assets/js/admin.js', ['jquery'], QTFW_VERSION, true);
-    }
 
     // Enqueue public scripts and styles
     public function enqueue_public_assets() {
-        wp_enqueue_style('qtfw-public', QTFW_PLUGIN_URL . 'assets/css/public.css', [], QTFW_VERSION);
-        wp_enqueue_script('qtfw-public', QTFW_PLUGIN_URL . 'assets/js/public.js', ['jquery'], QTFW_VERSION, true);
+        wp_enqueue_style('qtfw-main', QTFW_PLUGIN_URL . 'assets/css/style.css', [], QTFW_VERSION);       
     }
 }
